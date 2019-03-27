bf2d10041eb36e4c78c4ecf109705de32f0c6a2d
Rakesh <rakeshigntu@gmail.com>
2019-03-28 01:24:31 +0530

    Added VowelOccurance.java File

---
+++ VowelOccurance.java
@@ -0,0 +1,47 @@
+package com.app.projectclass.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Rakesh 
+ * this class is finding Occurrence of all Vowels
+ */
+
+public class VowelOccurance {
+	static void charCount(String str) {
+		HashMap<Character, Integer> hM = new HashMap<Character, Integer>();
+		char[] chrArray = str.toCharArray();
+
+		for (char c : chrArray) {
+			if (c =='a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' 
+				|| c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+			{
+				if (hM.containsKey(c)) {
+					hM.put(c, hM.get(c) + 1);
+				} else {
+					hM.put(c, 1);
+				}
+			} 
+		}
+		if(hM.size()==0) {
+			System.out.println("No Vowel Found In This Sentence ");
+		}
+		for (Map.Entry<Character, Integer> entry : hM.entrySet()) {
+			System.out.println("Volwel " + entry.getKey() + " is Occur => "
+										 + entry.getValue() + " Time ");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter Any Sentence : ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println("Your Sentence is : " + str);
+		System.out.println("---------------------------------------------");
+		charCount(str);
+	}
+}
