e02cc647d2280a816258001885525a735927bef4
Dadu95 <30949380+Dadu95@users.noreply.github.com>
2019-04-08 01:08:08 +0530

    Add files via upload
    added new

---
+++ JUnit-PowerMock-Contact-App/src/main/java/com/nit/dao/ContactDao.java
@@ -0,0 +1,8 @@
+package com.nit.dao;
+
+import com.nit.model.Contact;
+
+public interface ContactDao {
+
+	public Contact findByName(String name);
+}
---
+++ JUnit-PowerMock-Contact-App/src/test/java/com/nit/service/test/ContactServiceTest.java
@@ -0,0 +1,56 @@
+package com.nit.service.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.nit.dao.ContactDao;
+import com.nit.model.Contact;
+import com.nit.service.ContactService;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(fullyQualifiedNames = "com.nit.service.*")
+public class ContactServiceTest {
+
+	// @Test
+	public void test_findByName() {
+
+		ContactDao dao = PowerMockito.mock(ContactDao.class);
+
+		PowerMockito.when(dao.findByName("Ashok")).thenReturn(new Contact("Ashok", 123456L));
+
+		ContactService service = new ContactService();
+		service.setDao(dao);
+
+		Contact c = service.getByName("Ashok");
+		System.out.println(c);
+		Assert.assertNotNull(c);
+	}
+
+	@Test
+	public void test_printMessage_staticMethod() {
+
+		String input = "Hello PowerMockito"; // 1
+		String expectation = "Hello PowerMockito"; // 2
+
+		PowerMockito.mockStatic(ContactService.class); // 3
+
+		PowerMockito.when(ContactService.printMessage1(input)).thenReturn(expectation); // 4
+
+		String actual = ContactService.printMessage1(input); // 5
+		Assert.assertEquals(expectation, actual); // 6
+	}
+
+	@Test
+	public void test_printMessage2_privateMethod() throws Exception {
+
+		ContactService mock = PowerMockito.spy(new ContactService());
+
+		String actual = (String) org.powermock.reflect.Whitebox.invokeMethod(mock, "printMessage2", "Hi");
+		Assert.assertEquals("Hi", actual);
+	}
+
+}
